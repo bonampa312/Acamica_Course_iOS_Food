e91e8d7c3c9c220d52dcb52689b9b2485ab50313
Santiago Romero Restrepo <santiago.romero@co-it00758.globant.com>
2018-04-02 12:09:56 -0500

    Fixes and minnor funcs - animation, alert hihghlighted

--- AcamicaCourseApp/ViewController.h
+++ AcamicaCourseApp/ViewController.h
@@ -13,5 +13,10 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 
+@property (weak, nonatomic) IBOutlet UIView *viewToAnimate;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewWidthConstraint;
+
+
 @end
 
--- AcamicaCourseApp/ViewController.m
+++ AcamicaCourseApp/ViewController.m
@@ -8,10 +8,12 @@
 
 #import "ViewController.h"
 #import "Food.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 
 @property (nonatomic, strong) NSMutableArray *array;
+@property BOOL *isViewGreen;
 
 @end
 
@@ -23,7 +25,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.textField.text = @"Hello world";
+    self.isViewGreen = false;
     self.imageView.backgroundColor = [UIColor redColor];
+    self.textField.text = [[NSUserDefaults standardUserDefaults] objectForKey:@"userName"];
     
 }
 
@@ -33,7 +37,59 @@
 }
 
 - (IBAction)onTestMethod:(id)sender {
-    self.imageView.backgroundColor = [UIColor blackColor];
+    self.imageView.backgroundColor = (self.imageView.backgroundColor == [UIColor blackColor]) ? [UIColor blueColor] : [UIColor blackColor];
 }
+
+- (void)textFieldDidEndEditing:(UITextField*) textField {
+    if (textField.text) {
+        [[NSUserDefaults standardUserDefaults] setObject:textField.text forKey:@"userName"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+}
+
+- (BOOL)textFieldShouldReturn: (UITextField*) textField {
+    [textField resignFirstResponder];
+    return true;
+}
+- (IBAction)showAlert:(id)sender {
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"This is a happy alert!" message: @"Don't you dare to press OK button." preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"The Hazelnut is planted");
+    }];
+    [alert addAction:defaultAction];
+    [self presentViewController:alert animated:true completion:nil];
+}
+
+- (IBAction)animateView:(id)sender {
+    if (self.isViewGreen) {
+        self.viewWidthConstraint.constant = 240.0f;
+        self.viewHeightConstraint.constant = 120.0f;
+        __weak ViewController *weakSelf = self;
+        [UIView animateWithDuration:0.5 animations:^{
+            [self.view layoutIfNeeded];
+            self.viewToAnimate.backgroundColor = [UIColor redColor];
+        } completion:^(BOOL finished) {
+            weakSelf.isViewGreen = false;
+            finished = true;
+        }];
+    } else {
+        self.viewWidthConstraint.constant = 300.0f;
+        self.viewHeightConstraint.constant = 150.0f;
+        __weak ViewController *weakSelf = self;
+        
+        [UIView animateWithDuration:0.5 animations:^{
+            [self.view layoutIfNeeded];
+            self.viewToAnimate.backgroundColor = [UIColor greenColor];
+        } completion:^(BOOL finished) {
+            weakSelf.isViewGreen = true;
+            finished = true;
+        }];
+        
+    }
+    
+}
+
 
 @end
